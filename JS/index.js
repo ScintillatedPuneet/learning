4b6709187704be63712547dd8994a4886470957f
puneetlodha1@gmail.com <puneet@tekzensys.com>
2023-11-01 17:43:51 +0530

    testing

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous">
+
+function SubForm (){
+			$.ajax({
+				url:"https://api.apispreadsheets.com/data/mdkfnUUzMrOwKvGx/",
+				type:"post",
+				data:$("#myForm").serializeArray(),
+				success: function(){
+					alert("Form Data Submitted :)")
+				},
+				error: function(){
+					alert("There was an error :(")
+				}
+			})
+		}
+</script>
